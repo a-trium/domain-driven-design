91ccd18ccb130bc5b8dca4ccf3a5664eb44b344e
donald.duk <donald.duk@kakaopaycorp.com>
2018-05-23 18:09:16 +0900

    dep 모두 삭제, 불필요한 코드 삭제(Reset)

--- implementation-duk/service-gateway/cmd/server/main.go
+++ implementation-duk/service-gateway/cmd/server/main.go
@@ -1,39 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/a-trium/domain-driven-design/implementation-duk/service-gateway/internal/infra"
-	"github.com/a-trium/domain-driven-design/implementation-duk/service-gateway/internal/web/config"
-	"github.com/a-trium/domain-driven-design/implementation-duk/service-gateway/internal/web/controller"
-	"github.com/gin-gonic/gin"
-)
-
 func main() {
-	router := gin.Default()
 
-	env := config.GetEnvironment()
-	//_ := config.GetLogger(env)
-	gorm := config.GetDatabase(env.DatabaseProperty)
-
-	userRepository := repository.NewUserRepository(gorm)
-	userController := controller.NewUserController(userRepository)
-
-	//user := domain.User{Name: "Jinzhu", Age: 18, Birthday: time.Now()}
-	//db.Create(&user)
-
-	// TODO : middleware / router refactoring
-	router.Use(dummyMiddleware)
-	router.GET("/ping", controller.HealthCheck)
-
-	v1 := router.Group("/v1")
-	{
-		v1.GET("/user/:userId", userController.User)
-	}
-
-	router.Run(":" + env.Port)
-}
-
-func dummyMiddleware(context *gin.Context) {
-	fmt.Println("Dummy Middleware start")
-	context.Next()
 }
--- implementation-duk/service-gateway/internal/web/controller/health_check_controller.go
+++
@@ -1,9 +0,0 @@
-package controller
-
-import "github.com/gin-gonic/gin"
-
-func HealthCheck(context *gin.Context) {
-	context.JSON(200, gin.H{
-		"message": "pong",
-	})
-}
